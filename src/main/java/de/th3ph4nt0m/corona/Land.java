115edb717eba9d3e2caa85fa436b262efaad5b55
Th3Ph4nt0m <accounts@th3ph4nt0m.de>
2020-03-23 20:22:39 +0100

    - init

---
+++ src/main/java/de/th3ph4nt0m/corona/Land.java
@@ -0,0 +1,14 @@
+package de.th3ph4nt0m.corona;
+
+public class Land{
+    //Parameter
+    //Einwohner (infiziert, gesund, verstorben, wieder gesund)
+    //Maßnahmen
+
+    //rundeSimulieren()
+    //getEinwohner()
+    //getContacts() int
+    //getAnsteckungswahrscheinlichkeit()
+    //genesen()
+
+}
---
+++ src/main/java/de/th3ph4nt0m/corona/Simulation.java
@@ -0,0 +1,5 @@
+package de.th3ph4nt0m.corona;
+
+public class Simulation{
+
+}
